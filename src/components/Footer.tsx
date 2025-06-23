26411a94a7a1908fd4644ba95566a84184fd5a4b
ahyeonbaek <4641090@naver.com>
2025-06-23 20:03:39 +0900

    푸터 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import style from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={style.footer_wrap}>
+      <p>Ⓒ 2025. ahyeon's portfolio. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
