1d1553ab055232cc8fc4fac4acdf020df61d20f4
ahyeonbaek <4641090@naver.com>
2025-06-23 20:05:47 +0900

    다크모드 context 추가

--- src/context/mode.tsx
+++ src/context/mode.tsx
@@ -1,5 +1,30 @@
-const mode = () => {
-  return <div></div>;
-};
+import { createContext, useEffect, useState } from "react";
 
-export default mode;
+interface ModeContextProps {
+  onClickIsDark: () => void;
+  isDark: boolean;
+}
+
+export const DarkModeContext = createContext<ModeContextProps>({
+  onClickIsDark: () => {},
+  isDark: false,
+});
+
+export const DarkModeProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  const onClickIsDark = () => {
+    setIsDark((prev) => !prev);
+  };
+
+  useEffect;
+  return (
+    <DarkModeContext.Provider value={{ onClickIsDark, isDark }}>
+      <div>{children}</div>
+    </DarkModeContext.Provider>
+  );
+};
