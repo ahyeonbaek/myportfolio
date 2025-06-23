6e82cad69c8cca9d413c2cd91eb64f171abb9be7
ahyeonbaek <4641090@naver.com>
2025-06-23 20:04:01 +0900

    provider,footer추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { DarkModeProvider } from "./context/mode";
+import { RefProvider } from "./context/ref";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <Outlet />
-    </>
+    <RefProvider>
+      <DarkModeProvider>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </DarkModeProvider>
+    </RefProvider>
   );
 }
 
